82fd99bfd5027ae09578c2cc9c8edff56e6ec803
zaoerck <zaoerck@163.com>
2015-05-19 22:31:57 +0800

    添加摇一摇

--- src/com/way/activity/MapActivity.java
+++ src/com/way/activity/MapActivity.java
@@ -1,6 +1,5 @@
 package com.way.activity;
 
-import com.way.bean.MessageItem;
 import com.way.bean.RecentItem;
 import com.way.common.util.SendMsgAsyncTask;
 import com.way.push.R;
@@ -8,22 +7,41 @@ import com.way.push.R.layout;
 import com.way.push.R.menu;
 
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MapActivity extends Activity implements OnClickListener{
+public class MapActivity extends Activity implements OnClickListener, SensorEventListener{
 	private TextView mTitle, mTitleLeftBtn;
-	
+	TextView tv = null;  
+    Button button = null;  
+    SensorManager sensorManager = null;  
+    Vibrator vibrator = null; 
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		initView();
+		
+		 
+        tv = (TextView) findViewById(R.id.mapTextView);  
+          
+  
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);  
+        vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE); 
 	}
 
 	@Override
@@ -51,13 +69,52 @@ public class MapActivity extends Activity implements OnClickListener{
 		switch (v.getId()) {
 		//点击返回跳转会主页面
 		case R.id.ivTitleBtnLeft:
-			startActivity(new Intent(getApplicationContext(), MainActivity.class));
+			finish();
 			break;
 		case R.id.ivTitleBtnRigh:
 			break;
 		default:
 			break;
 		}
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		int sensorType = event.sensor.getType();  
+        //values[0]:X轴，values[1]：Y轴，values[2]：Z轴  
+        float[] values = event.values;  
+        if (sensorType == Sensor.TYPE_ACCELEROMETER)  
+        {  
+            if ((Math.abs(values[0]) > 17 || Math.abs(values[1]) > 17 || Math  
+                    .abs(values[2]) > 17))  
+            {  
+                Log.d("sensor x ", "============ values[0] = " + values[0]);  
+                Log.d("sensor y ", "============ values[1] = " + values[1]);  
+                Log.d("sensor z ", "============ values[2] = " + values[2]);  
+                tv.setText("摇一摇成功!!!");
+                tv.setTextSize(16);
+                tv.setTextColor(Color.RED);
+                //摇动手机后，再伴随震动提示~~  
+                vibrator.vibrate(500);  
+            }  
+  
+        }  
+	}
+	
+	@Override  
+    protected void onResume()  
+    {  
+        super.onResume();  
+        sensorManager.registerListener(this,  
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),  
+                SensorManager.SENSOR_DELAY_NORMAL);  
+    } 
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		//当传感器精度改变时回调该方法，Do nothing. 
 	}
 
 }
